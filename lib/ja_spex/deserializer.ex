5379cd8efb6f55c4e5c9dda701a55668e67f3fb0
Michael Crumm <mike@crumm.net>
2019-07-01 10:27:33 -0700

    update deserializer and plug

--- lib/ja_spex/deserializer.ex
+++ lib/ja_spex/deserializer.ex
@@ -1,97 +1,56 @@
 defmodule JaSpex.Deserializer do
   @moduledoc """
-  When used on a Schema, deserializes derived structs into attribute maps.
+  A replacement for Plug `JaSerializer.Deserializer`.
+
+  Add `JaSpex.Deserializer` to your pipeline when you want to
+  parse the request parameters as JSON-API _after_ using cast and
+  validate from `OpenApiSpex`.
+
+  It replaces `JaSerializer.Deserializer` because OpenApiSpex will
+  split the `params` and `body_params` in the conn struct, and the
+  base Deserializer only works with params.
+
+  Due to this difference in functionality, `JaSpex.Deserializer`
+  should be included in your pipeline _after_ `OpenApiSpex.Plug.Validate`.
+
+  To handle the validation and deserialization in one step, you can use
+  `plug(JaSpex)` instead.
   """
+  @behaviour Plug
+  alias JaSerializer.ParamParser
 
-  @doc false
-  # Not to be directly implemented, use the `from_schema/2` macro instead
-  @callback __from_schema__(data :: map) :: map
+  @impl Plug
+  @spec init(Plug.opts()) :: Plug.opts()
+  def init(opts \\ []), do: Keyword.take(opts, [:merge_params])
 
-  @doc false
-  defmacro __using__(opts \\ []) do
-    quote do
-      require JaSpex.Deserializer
-      import JaSpex.Deserializer, only: [from_schema: 2]
-      @behaviour JaSpex.Deserializer
-
-      @doc false
-      def __parser_opts__, do: unquote(opts)
-
-      def __from_schema__(schema), do: schema
-
-      defoverridable __from_schema__: 1
-
-      @after_compile {JaSpex.SchemaParser, :__compile_parser__}
-    end
+  @impl Plug
+  @spec call(Plug.Conn.t(), Plug.opts()) :: Plug.Conn.t()
+  def call(%Plug.Conn{} = conn, opts) do
+    conn
+    |> parse_params()
+    |> parse_body_params()
+    |> maybe_merge_params(opts)
   end
 
-  defmacro from_schema(param, do: block) do
-    quote do
-      def __from_schema__(unquote(param)) do
-        import JaSpex.Deserializer, except: [from_schema: 2]
-        unquote(block)
-      end
-    end
+  defp parse_params(conn) do
+    params = Enum.into(conn.params, %{}, &cast_param_key/1)
+
+    Map.put(conn, :params, ParamParser.parse(params))
   end
 
-  @doc """
-  Casts a datetime from the `field` to the given `type`.
+  # Cast param keys back to strings prior to parsing
+  defp cast_param_key({k, v}) when is_atom(k), do: {to_string(k), v}
+  defp cast_param_key(other), do: other
 
-  ## Examples
-
-      iex> %{a: DateTime.from_naive!(~N[2015-01-13 13:00:07.987654], "Etc/UTC")}
-      ...> |> JaSpex.Deserializer.cast_datetime(:a, :naive_datetime)
-      %{a: ~N[2015-01-13 13:00:07]}
-
-      iex> %{a: DateTime.from_naive!(~N[2015-01-13 13:00:07.123456], "Etc/UTC")}
-      ...> |> JaSpex.Deserializer.cast_datetime(:a, :naive_datetime_usec)
-      %{a: ~N[2015-01-13 13:00:07.123456]}
-  """
-  def cast_datetime(data, field, type)
-
-  def cast_datetime(%{} = data, key, type) do
-    key = List.wrap(key)
-    maybe_update_in(data, key, maybe(&cast_datetime_to(&1, type)))
+  defp parse_body_params(conn) do
+    Map.put(conn, :body_params, ParamParser.parse(conn.body_params))
   end
 
-  defp cast_datetime_to(%DateTime{} = datetime, type) when is_atom(type),
-    do: datetime_to(datetime, type)
-
-  defp cast_datetime_to(value, _), do: value
-
-  defp datetime_to(datetime, :naive_datetime) do
-    naive_datetime = DateTime.to_naive(datetime)
-
-    %{naive_datetime | microsecond: {0, 0}}
+  defp maybe_merge_params(conn, merge_params: true) do
+    Map.put(conn, :params, Map.merge(conn.params, conn.body_params))
   end
 
-  defp datetime_to(datetime, :naive_datetime_usec) do
-    DateTime.to_naive(datetime)
-  end
-
-  defp datetime_to(datetime, :utc_datetime) do
-    {:ok, utc_datetime} = DateTime.shift_zone(datetime, "Etc/UTC")
-
-    %{utc_datetime | microsecond: {0, 0}}
-  end
-
-  defp datetime_to(datetime, :utc_datetime_usec) do
-    {:ok, utc_datetime} = DateTime.shift_zone(datetime, "Etc/UTC")
-
-    utc_datetime
-  end
-
-  defp maybe(fun) do
-    fn
-      nil -> nil
-      arg -> fun.(arg)
-    end
-  end
-
-  defp maybe_update_in(data, path, fun) do
-    case get_in(data, path) do
-      nil -> data
-      val -> put_in(data, path, fun.(val))
-    end
+  defp maybe_merge_params(conn, _) do
+    conn
   end
 end
